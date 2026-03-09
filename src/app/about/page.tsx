dcdb5ddfd6b90106b19ae3064f200f226b8478c5
althea rosales <altheashewrote@altheas-DAW-3.local>
2026-03-09 14:45:12 -0700

    h

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
     return (
         <div className="flex flex-col min-h-screen items-center justify-center bg-rave-black font-sans dark:bg-rave-black">
             <main className="relative w-screen h-screen">
-
+                
             </main>
         </div>
     )
