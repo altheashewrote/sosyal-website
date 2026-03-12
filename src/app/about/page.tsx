6e9bdb55781c7f51bf4640ebb3b3a3a4d9ea7427
althea rosales <altheashewrote@altheas-DAW-3.local>
2026-03-12 11:33:08 -0700

    implemented whole timeline, 2.0

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -38,18 +38,24 @@ export default function About() {
                 <div className="relative flex flex-row justify-between gap-10">
                     <div className="flex flex-col items-start gap-10">
                         <TimelineCard
-                        date="26 January 2025"
+                        date="24 January 2025"
                         name="Boba & Bass"
                         desc="Where it all began, featuring the one and only SNDR (pronounced 'sonder')."
-                        image="/images/events/two_pretty_best_friends.jpg"
+                        image="/images/events/boba_bass_01.jpg"
                         />
+                        <div className="font-helvetica max-h-140 text-ara-white text-right flex flex-col gap-6 pl-30 py-30 opacity-0">
+                            <div>
+                                <h3 className="font-bold uppercase">22 November 2025</h3>
+                                <p className="font-medium uppercase tracking-wide">Operation Knockout: Unofficial Knock2 Afters</p>
+                            </div>
+                        </div>
                         <TimelineCard
                         date="9 May 2025"
                         name="Studios Vol. 1"
                         desc="Launched a series of intimate DJ sets with feral energy, brought to you by the Gratitude. Featuring Desert Hydra."
-                        image="/images/events/two_pretty_best_friends.jpg"
+                        image="/images/events/studios_dh_01.jpg"
                         />
-                        <div className="font-helvetica max-h-140 text-ara-white text-right flex flex-col gap-6 pl-54 py-25">
+                        <div className="font-helvetica max-h-140 text-ara-white text-right flex flex-col gap-6 pl-54 py-70">
                             <div>
                                 <h3 className="font-bold uppercase">7 SEPTEMBER 2025</h3>
                                 <p className="font-medium uppercase tracking-wide">SOSYAL takes over Galactic Gardens</p>
@@ -59,9 +65,9 @@ export default function About() {
                         date="9 September 2025"
                         name="Discord Launch"
                         desc="A new form of SOSYALISING is here."
-                        image="/images/events/two_pretty_best_friends.jpg"
+                        image="/images/events/three_founders_01.jpg"
                         />
-                        <div className="font-helvetica max-h-140 text-ara-white text-right flex flex-col gap-6 pl-30 py-25">
+                        <div className="font-helvetica max-h-140 text-ara-white text-right flex flex-col gap-6 pl-30 py-60">
                             <div>
                                 <h3 className="font-bold uppercase">22 November 2025</h3>
                                 <p className="font-medium uppercase tracking-wide">Operation Knockout: Unofficial Knock2 Afters</p>
@@ -71,8 +77,14 @@ export default function About() {
                         date="19 December 2025"
                         name="Bass City"
                         desc="The culmination of months of community-building, trial and error, and throwing down. Our Reverb SF debut, featuring DJ KANG, Shogun, and IKU B2B CHRISPYNOY."
-                        image="/images/events/two_pretty_best_friends.jpg"
+                        image="/images/events/chrispinoy_iku_2.jpg"
                         />
+                        <div className="font-helvetica max-h-140 text-ara-white text-right flex flex-col gap-6 pl-30 py-50 opacity-0">
+                            <div>
+                                <h3 className="font-bold uppercase">22 November 2025</h3>
+                                <p className="font-medium uppercase tracking-wide">Operation Knockout: Unofficial Knock2 Afters</p>
+                            </div>
+                        </div>
                         <TimelineCard
                         date="14 March 2026"
                         name="After Snow"
@@ -80,9 +92,9 @@ export default function About() {
                         image="/images/events/two_pretty_best_friends.jpg"
                         />
                     </div>
-                    <div className="w-0.5 h-880 bg-chromeangels-grey"></div>
+                    <div className="w-0.5 h-1500 bg-chromeangels-grey"></div>
                     <div className="flex flex-col gap-10">
-                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-25">
+                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-30">
                             <div>
                                 <h3 className="font-bold uppercase">28 FEBRUARY 2025</h3>
                                 <p className="font-medium uppercase tracking-wide">Boba and Bass II featuring SNDR and ARATHENA</p>
@@ -92,9 +104,9 @@ export default function About() {
                         date="11 April 2025"
                         name="Labubass"
                         desc="Setting the stage for the first time at Concord's Gratitude Lounge. Featuring CHRISPYNOY, Harabaz, and DRO."
-                        image="/images/events/two_pretty_best_friends.jpg"
+                        image="/images/events/labubass_01.jpg"
                         />
-                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-5">
+                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-30">
                             <div>
                                 <h3 className="font-bold uppercase">7 JUNE 2025</h3>
                                 <p className="font-medium uppercase tracking-wide">Barrio Rave featuring CHRISPYNOY</p>
@@ -117,9 +129,9 @@ export default function About() {
                         date="16 August 2025"
                         name="Studios Vol. 4"
                         desc="The best for last. Our final iteration of Studios for 2025, featuring $J, Hapazin, and VICEVERSA."
-                        image="/images/events/two_pretty_best_friends.jpg"
+                        image="/images/events/studios_hz_04.jpg"
                         />
-                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-5">
+                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-50">
                             <div>
                                 <h3 className="font-bold uppercase">10-11 OCTOBER 2025</h3>
                                 <p className="font-medium uppercase tracking-wide">Unofficial Niteharts Pre-Party and Afters, featuring IKU</p>
@@ -133,9 +145,9 @@ export default function About() {
                         date="Halloween Weekend 2025"
                         name="Late Night Madness"
                         desc="Who said you couldn't go feral at Seafood City?"
-                        image="/images/events/two_pretty_best_friends.jpg"
+                        image="/images/events/ara_sfc.jpg"
                         />
-                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-5">
+                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-40">
                             <div>
                                 <h3 className="font-bold uppercase">17 JANUARY 2026</h3>
                                 <p className="font-medium uppercase tracking-wide">Sosyal 360 — our LA debut with $J</p>
@@ -151,7 +163,7 @@ export default function About() {
                         desc="Our Continental Club debut, featuring $J, IKU, and headbanging lion dancers from SJSU VSA. Happy Lunar New Year!"
                         image="/images/events/lion_headbang.jpg"
                         />
-                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-5">
+                        <div className="font-helvetica max-h-140 text-ara-white flex flex-col gap-6 py-50">
                             <div>
                                 <h3 className="font-bold uppercase">26 MARCH 2026</h3>
                                 <p className="font-medium uppercase tracking-wide">CSUF Beyond Pre-Party with SARDONIX and CHRISPYNOY</p>
