d359363d3b9f91673c33d405fcce34025aa9d101
althea rosales <altheashewrote@altheas-DAW-3.local>
2026-04-02 13:42:42 -0700

    fixed email question mark?

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,10 +1,9 @@
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 export async function POST(req: Request) {
     console.log('Contact API route hit')
     try {
+        const resend = new Resend(process.env.RESEND_API_KEY)
         const { name, email, message } = await req.json()
 
         await resend.emails.send({
