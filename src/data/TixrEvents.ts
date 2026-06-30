223e6309a3e61e3cf3c7c08253ca617486a1ea14
Althea Rosales <whoatherealthea@berkeley.edu>
2026-06-30 12:40:00 -0700

    whoops sorry fixed the date on vanity

--- src/data/TixrEvents.ts
+++ src/data/TixrEvents.ts
@@ -4,8 +4,8 @@ export const tixrEvents: Event[] = [
     {
         id: '185255',
         name: 'VANITY FASHION SHOW RAVE',
-        start: '2026-08-08T04:00:00.000Z',
-        end: '2026-08-08T09:00:00.000Z',
+        start: '2026-08-09T04:00:00.000Z',
+        end: '2026-08-09T09:00:00.000Z',
         timezone: 'America/Los_Angeles',
         status: 'live',
         flyer: 'https://oaklandcontinentalclub.com/wp-content/uploads/Screenshot-2026-06-29-at-8.43.19-PM.png.webp',
